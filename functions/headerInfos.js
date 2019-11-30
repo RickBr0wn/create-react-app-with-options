a1532d5e540be45b9294f1066fb975571fcc26f2
Rick Brown <ricky.brown.00@gmail.com>
2019-11-30 02:26:37 +0000

    improved console.info information

--- functions/headerInfos.js
+++ functions/headerInfos.js
@@ -6,7 +6,7 @@ module.exports = function headerInfos() {
   console.info('Welcome to create-react-app-with-options.'.green);
   console.info('');
   console.info(
-    "This cli tool allows you to customize the create-react-app process. You will be asked a series of questions relevant to the npm packages you require. It will then use create-react-app in it's usual manner but will install all of the required dependencies and replace the create-react-app boilerplate with a 'trimmed' down boilerplate bespoke to your dependencies and answers."
+    "This cli tool allows you to customize the create-react-app process. You will be asked a series of questions relevant to the app and the npm packages you require. It will then use create-react-app in it's usual manner but will install all of the required dependencies and replace the create-react-app boilerplate with a 'trimmed' down boilerplate bespoke to your dependencies and answers."
   );
   console.info('');
 };
